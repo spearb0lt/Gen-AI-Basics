690dc0c2ae37b61d108308a920fa27dcf0c004f8
Shubhro Dev <84016735+spearb0lt@users.noreply.github.com>
2025-07-05 13:19:27 +0530

    Update main.py

--- RNN_imdb/main.py
+++ RNN_imdb/main.py
@@ -9,7 +9,7 @@ import os
 word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, 'simple_rnn_imdb.h5')
+model_path = os.path.join(BASE_DIR, 'simple_rnn_imdb_savedmodel.keras')
 # Load the pre-trained model with ReLU activation
 model = tf.keras.models.load_model(model_path)
 
